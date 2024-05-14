8f38d78e95499802185368979062fc029184147f
Piotr Fidkowski <piotr@fidkowski.com>
2024-05-14 15:48:51 -0700

    Upgraded scraper tool to use absl flags.

--- poldict/scrape_test_data.py
+++ poldict/scrape_test_data.py
@@ -1,9 +1,33 @@
+import os.path
 import sys
+
+from absl import app
+from absl import flags
 
 import wiktionary_scraper
 
+FLAGS=flags.FLAGS
+flags.DEFINE_boolean('rescrape_all', False, 'If true, rescrape all test cases')
+flags.DEFINE_list('words',
+                  ['biegać',
+                   'czerwony',
+                   'drzwi',
+                   'gość',
+                   'pobiec'],
+                  'Words to scrape')
+
+
+def main(argv):
+    del argv
+    for word in FLAGS.words:
+      filename = f'testdata/{word}.html'
+      if os.path.exists(filename) and not FLAGS.rescrape_all:
+          continue
+      print('Scraping html for "{}"'.format(word))
+      html = wiktionary_scraper.get_html(word)
+      open(filename, 'w').write(html.decode('utf-8'))    
+
+
 if __name__ == "__main__":
-    word = sys.argv[1]
-    print('Scraping html for "{}"'.format(word))
-    html = wiktionary_scraper.get_html(word)
-    open('testdata/' + word + '.html', 'w').write(html.decode('utf-8'))
+    app.run(main)
+    
