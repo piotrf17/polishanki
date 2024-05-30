f4ffe3e973017a594825d39f002be2bd5883f46e
Piotr Fidkowski <piotr@fidkowski.com>
2024-05-30 09:16:17 -0700

    Refactored note management to a separate class.

--- client/src/NoteList.jsx
+++ client/src/NoteList.jsx
@@ -2,103 +2,42 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 import Note from "./Note";
+import NoteService from "./services/notes";
 
 const NoteList = ({ word, noteFilter = (n) => true }) => {
   const [notes, setNotes] = useState([]);
+  const noteService = new NoteService(notes, setNotes, noteFilter);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/notes_for_word/${word}`)
-      .then((response) => {
-        setNotes(
-          response.data
-            .map((n) => ({ data: n, editing: false }))
-            // Sort to show most recent cards first.
-            .sort((a, b) => b.data.createdTs - a.data.createdTs)
-            .filter(noteFilter)
-        );
-      });
+    noteService.getNotes(word);
   }, []);
-
-  const addNote = () => {
-    // Only allow 1 new note at a time.
-    if (notes.length > 1 && notes[0].data.id == 0) {
-      alert("only one new note at a time");
-      return;
-    }
-    let newNote = {
-      data: {
-        id: 0,
-        createdTs: Date.now() / 1000.0,
-        word: word,
-      },
-      editing: true,
-    };
-    setNotes([newNote].concat(notes));
-  };
-
-  // TODO(piotrf): should we verify that changedData still passes noteFilter?
-  const updateData = (changedData) => {
-    const id = changedData.id;
-    if (id == 0) {
-      // If this is a new note, post it to the server and update the ID.
-      delete changedData.id;
-      axios
-        .post("http://localhost:5000/api/notes", changedData)
-        .then((response) => {
-          const newNote = {
-            data: { ...changedData, id: response.data.id },
-            editing: false,
-          };
-          setNotes([newNote].concat(notes.slice(1)));
-        });
-    } else {
-      // Otherwise, just update the data.
-      const url = `http://localhost:5000/api/notes/${id}`;
-      axios.put(url, changedData).then((response) => {
-        setNotes(
-          notes.map((n) =>
-            n.data.id == id ? { data: changedData, editing: false } : n
-          )
-        );
-      });
-    }
-  };
-
-  const setEditing = (id, editing) => {
-    // If we cancel a pending new note, remove it from the list.
-    if (id == 0) {
-      setNotes(notes.slice(1));
-    } else {
-      setNotes(
-        notes.map((n) => (n.data.id == id ? { ...n, editing: editing } : n))
-      );
-    }
-  };
-
-  const handleDelete = (id) => {
-    if (confirm("Really delete?")) {
-      const url = `http://localhost:5000/api/notes/${id}`;
-      axios.delete(url).then((response) => {
-        setNotes(notes.filter((n) => n.data.id != id));
-      });
-    }
-  };
 
   return (
     <div>
       <h2>Notes ({notes.length})</h2>
       <div>
-        <button onClick={addNote}>Add a new note</button>
+        <button
+          onClick={() => {
+            noteService.addNote(word);
+          }}
+        >
+          Add a new note
+        </button>
       </div>
       {notes.map((n) => (
         <Note
           key={n.data.id}
           data={n.data}
           editing={n.editing}
-          updateData={updateData}
-          setEditing={setEditing.bind(null, n.data.id)}
-          handleDelete={handleDelete.bind(null, n.data.id)}
+          updateData={(changedData) => {
+            noteService.updateNoteData(changedData);
+          }}
+          setEditing={(editing) => {
+            noteService.setEditing(n.data.id, editing);
+          }}
+          handleDelete={() => {
+            noteService.deleteNote(n.data.id);
+          }}
         />
       ))}
     </div>
---
+++ client/src/services/notes.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000/api";
+
+class NoteService {
+  constructor(notes, setNotes, noteFilter = (n) => true) {
+    this.notes = notes;
+    this.setNotes = setNotes;
+    this.noteFilter = noteFilter;
+  }
+
+  getNotes(word) {
+    axios.get(`${baseUrl}/notes_for_word/${word}`).then((response) => {
+      this.setNotes(
+        response.data
+          .map((n) => ({ data: n, editing: false }))
+          // Sort to show most recent cards first.
+          .sort((a, b) => b.data.createdTs - a.data.createdTs)
+          .filter(this.noteFilter)
+      );
+    });
+  }
+
+  addNote(word, data = {}) {
+    // Only allow 1 new note at a time.
+    if (this.notes.length > 1 && this.notes[0].data.id == 0) {
+      alert("only one new note at a time");
+      return;
+    }
+    let newNote = {
+      data: {
+        ...data,
+        id: 0,
+        createdTs: Date.now() / 1000.0,
+        word: word,
+      },
+      editing: true,
+    };
+    this.setNotes([newNote].concat(this.notes));
+  }
+
+  // TODO(piotrf): should we verify that changedData still passes noteFilter?
+  updateNoteData(changedData) {
+    const id = changedData.id;
+    if (id == 0) {
+      // If this is a new note, post it to the server and update the ID.
+      delete changedData.id;
+      axios.post(`${baseUrl}/notes`, changedData).then((response) => {
+        const newNote = {
+          data: { ...changedData, id: response.data.id },
+          editing: false,
+        };
+        this.setNotes([newNote].concat(this.notes.slice(1)));
+      });
+    } else {
+      // Otherwise, just update the data.
+      const url = `${baseUrl}/notes/${id}`;
+      axios.put(url, changedData).then((response) => {
+        this.setNotes(
+          this.notes.map((n) =>
+            n.data.id == id ? { data: changedData, editing: false } : n
+          )
+        );
+      });
+    }
+  }
+
+  setEditing(id, editing) {
+    // If we cancel a pending new note, remove it from the list.
+    if (id == 0) {
+      this.setNotes(this.notes.slice(1));
+    } else {
+      this.setNotes(
+        this.notes.map((n) =>
+          n.data.id == id ? { ...n, editing: editing } : n
+        )
+      );
+    }
+  }
+
+  deleteNote(id) {
+    if (confirm("Really delete?")) {
+      const url = `${baseUrl}/notes/${id}`;
+      axios.delete(url).then((response) => {
+        this.setNotes(this.notes.filter((n) => n.data.id != id));
+      });
+    }
+  }
+}
+
+export default NoteService;
